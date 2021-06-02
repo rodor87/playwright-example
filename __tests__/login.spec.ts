576548258320fbff25cb0962de7b5aac1225d66d
rodor87 <dimakos.neoklis@gmail.com>
2021-06-02 16:23:22 +0100

    Add setup and teardown hooks

--- __tests__/login.spec.ts
+++ __tests__/login.spec.ts
@@ -1,13 +1,29 @@
-import { chromium } from 'playwright';
+import { Browser, BrowserContext, chromium, Page } from 'playwright';
+
+let browser: Browser;
+let page: Page;
+let context: BrowserContext;
+
+beforeAll(async () => {
+	browser = await chromium.launch({ headless: true });
+	page = await browser.newPage();
+});
+
+afterAll(async () => {
+	browser.close();
+});
+
+beforeEach(async () => {
+	context = await browser.newContext();
+});
+
+afterEach(async () => {
+	context.close();
+});
 
 describe('Login to saucelabs "The Internet" website', () => {
-  it('Should successfully login when valid credentials are used', async () => {
-    const browser = await chromium.launch({ headless: true });
-    const page = await browser.newPage();
-    const context = await browser.newContext();
-    await page.goto('https://the-internet.herokuapp.com/login');
-    await page.screenshot();
-    await context.close();
-    browser.close();
-  });
+	it('Should successfully login when valid credentials are used', async () => {
+		await page.goto('https://the-internet.herokuapp.com/login');
+		await page.fill('id=username', 'test');
+	});
 });
